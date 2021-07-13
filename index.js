cff96ec673c2c1cb63e266c974a99872d0d70a04
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-13 09:12:49 -0700

    centered cards

--- index.js
+++ index.js
@@ -24,35 +24,9 @@ function displayCoins(coinArray) {
     let oneCard = document.createElement('div')
     let coinPrice = parseFloat(Number(element['priceUsd'])).toFixed(2)
     let chartDiv = document.createElement('div')
-    chartDiv.innerHTML = 
-    `<!-- TradingView Widget BEGIN -->
-    <div class="tradingview-widget-container">
-      <div id="tradingview_fead9"></div>
-      <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/${element['symbol']}/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
-      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-      <script type="text/javascript">
-      new TradingView.widget(
-      {
-      "autosize": true,
-      "symbol": "${element['symbol']}",
-      "interval": "D",
-      "timezone": "Etc/UTC",
-      "theme": "light",
-      "style": "1",
-      "locale": "en",
-      "toolbar_bg": "#f1f3f6",
-      "enable_publishing": false,
-      "hide_top_toolbar": true,
-      "hide_legend": true,
-      "save_image": false,
-      "container_id": "tradingview_fead9"
-    }
-      );
-      </script>
-    </div>
-    <!-- TradingView Widget END -->`
+    chartDiv.innerHTML = ""
     oneCard.setAttribute('id', `${element[`symbol`]}`)
-    oneCard.setAttribute('class', 'coinCard')
+    oneCard.setAttribute('class', 'coin-card')
     let text = document.createElement('h1')
     text.textContent = `#${element['rank']} | ${element[`name`]} | $${coinPrice}`
     cards.appendChild(oneCard)
