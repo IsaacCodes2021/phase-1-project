04d1630b31d99fee1bf7ad611c3b7157dca78eed
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-13 10:23:46 -0700

    added chart & changed ID of card to name

--- index.js
+++ index.js
@@ -24,8 +24,35 @@ function displayCoins(coinArray) {
     let oneCard = document.createElement('div')
     let coinPrice = parseFloat(Number(element['priceUsd'])).toFixed(2)
     let chartDiv = document.createElement('div')
-    chartDiv.innerHTML = ""
-    oneCard.setAttribute('id', `${element[`symbol`]}`)
+    chartDiv.innerHTML = `<!-- TradingView Widget BEGIN -->
+    <div class="tradingview-widget-container" style = "height: 363px">
+      <div id="tradingview_537d0"></div>
+      <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+      <script type="text/javascript">
+      new TradingView.widget(
+      {
+      "width": 400,
+      "height": 363,
+      "symbol": "NASDAQ:AAPL",
+      "interval": "15",
+      "timezone": "America/Los_Angeles",
+      "theme": "dark",
+      "style": "2",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "hide_top_toolbar": true,
+      "hide_legend": true,
+      "save_image": false,
+      "container_id": "tradingview_537d0"
+    }
+      );
+      </script>
+    </div>
+    <!-- TradingView Widget END -->`
+    chartDiv.setAttribute('class', 'chart-container')
+    oneCard.setAttribute('id', `${element[`name`]}`)
     oneCard.setAttribute('class', 'coin-card')
     let text = document.createElement('h1')
     text.textContent = `#${element['rank']} | ${element[`name`]} | $${coinPrice}`
