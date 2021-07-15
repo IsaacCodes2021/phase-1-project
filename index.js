2c2478d420b0931b838af13efbc8b73bcd4e94d5
IsaacCodes2021 <isaac.businessiq@gmail.com>
2021-07-15 16:32:48 -0700

    scroll to top button

--- index.js
+++ index.js
@@ -292,3 +292,22 @@ function biggestChange(coinArray){
 function smoothScroll(){
     document.querySelectorAll('a').addEventListener('click', )
 }
+// scroll to top 
+const mybutton = document.getElementById("myBtn");
+body.appendChild(mybutton)
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome
+}
